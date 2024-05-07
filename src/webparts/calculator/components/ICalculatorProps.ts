edd03fbbff02ff165d4ee99859dc25cd01764a21
Gaya <91261240+sdooodly@users.noreply.github.com>
2024-05-07 12:54:27 +0530

    Add files via upload

---
+++ src/webparts/calculator/components/ICalculatorProps.ts
@@ -0,0 +1,7 @@
+export interface ICalculatorProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/calculator/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICalculatorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CalculatorWebPartStrings' {
+  const strings: ICalculatorWebPartStrings;
+  export = strings;
+}
